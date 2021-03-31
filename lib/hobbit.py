358d7a611f757b75f96626b20787f000e9ae22bf
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 16:24:59 -0400

    Finish hobbit exercise

--- lib/hobbit.py
+++ lib/hobbit.py
@@ -1,1 +1,20 @@
+class Hobbit:
+  def __init__(self, name, disposition = "homebody"):
+    self.name = name
+    self.disposition = disposition
+    self.age = 0
 
+  def celebrate_birthday(self):
+    self.age += 1
+
+  def is_adult(self):
+    return True if self.age > 32 else False
+
+  def is_old(self):
+    return True if self.age >= 101 else False
+
+  def has_ring(self):
+    return True if self.name == "Frodo" else False
+
+  def is_short(self):
+    return True
