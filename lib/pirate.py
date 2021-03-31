62d59eb5a4dd47b269f798a47ec189e981c19415
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 16:30:00 -0400

    Finish pirate exercise

--- lib/pirate.py
+++ lib/pirate.py
@@ -1,1 +1,15 @@
+class Pirate:
+  def __init__(self, name, job = "Scallywag"):
+    self.name = name
+    self.job = job
+    self.count = 0
+    self.booty = 0
 
+  def is_cursed(self):
+    return False if self.count < 3 else True
+
+  def commit_heinous_act(self):
+    self.count += 1
+
+  def rob_ship(self):
+    self.booty += 100
