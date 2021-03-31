47d68bfe10b9aa54cd05eebcf2a97245ffb75f8e
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 15:52:49 -0400

    Finish dragon exercise

--- lib/dragon.py
+++ lib/dragon.py
@@ -1,1 +1,13 @@
 
+class Dragon:
+  def __init__(self, name, color, rider):
+    self.name = name
+    self.color = color
+    self.rider = rider
+    self.count = 0
+
+  def eat(self):
+    self.count += 1
+
+  def is_hungry(self):
+    return True if self.count < 3 else False
