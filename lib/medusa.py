f3dc8b29ca3c3de1fe96ddd5dc0efdad13a6833a
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 17:13:59 -0400

    Finish medusa

--- lib/medusa.py
+++ lib/medusa.py
@@ -1,1 +1,25 @@
+class Medusa:
+  def __init__(self, name,):
+    self.name = name
+    self.statues = []
+    self.count = 0
+
+  def stare(self, person):
+    if len(self.statues) == 3:
+      self.statues[0].stoned_status = False
+      self.statues.pop(0)
+      person.stoned_status = True
+      self.statues.append(person)
+    else:
+      person.stoned_status = True
+      self.statues.append(person)
+
+
+class Person:
+  def __init__(self, name, stoned_status = False):
+    self.name = name
+    self.stoned_status = stoned_status
+
+  def is_stoned(self):
+    return self.stoned_status
 
