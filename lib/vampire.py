1b8f0ddd85de445e5d77cbe75fb25151c1fc6af1
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 15:59:01 -0400

    Finish vampire exercise

--- lib/vampire.py
+++ lib/vampire.py
@@ -1,1 +1,11 @@
+class Vampire:
+  def __init__(self, name, pet = "bat"):
+    self.name = name
+    self.pet = pet
+    self.count = 0
 
+  def is_thirsty(self):
+    return True if self.count == 0 else False
+
+  def drink(self):
+    self.count += 1
