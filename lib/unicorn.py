36d42df6f797a00de97d4e570667dd7794b7d464
Kevin Cuadros <kev040798@gmail.com>
2021-03-31 15:46:03 -0400

    Finish unicorn exercise

--- lib/unicorn.py
+++ lib/unicorn.py
@@ -1,1 +1,10 @@
+class Unicorn:
+  def __init__(self, name, color = "white"):
+    self.name = name
+    self.color = color
 
+  def is_white(self):
+    return True if self.color == "white" else False
+
+  def say(self, word):
+    return f"**;* {word} **;*"
